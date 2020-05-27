4361a6ca90d6ef4eb59a5e0ceea05066a56b4de4
Mona Vorschulze <>
2020-05-27 14:49:55 +0200

    Create Controller and Service

--- src/main/java/de/neuefische/springgroupproject1/controller/OrderController.java
+++ src/main/java/de/neuefische/springgroupproject1/controller/OrderController.java
@@ -4,9 +4,7 @@ package de.neuefische.springgroupproject1.controller;
 import de.neuefische.springgroupproject1.model.Order;
 import de.neuefische.springgroupproject1.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,12 @@ private final OrderService orderService;
     @GetMapping
     public List<Order> getOrderList (){
         return orderService.getOrderList();
+    }
+
+    @PutMapping
+    public Order addOrder(@RequestBody Order order){
+        orderService.addOrder(order);
+    return order;
     }
 
 }
--- src/main/java/de/neuefische/springgroupproject1/db/OrderDB.java
+++ src/main/java/de/neuefische/springgroupproject1/db/OrderDB.java
@@ -1,25 +1,28 @@
 package de.neuefische.springgroupproject1.db;
 
 import de.neuefische.springgroupproject1.model.Order;
+import de.neuefische.springgroupproject1.model.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class OrderDB {
 
-    private final List<Order> orderList = new ArrayList<>();
+    private final List<Order> orderList = new ArrayList<>(List.of(
+
+            new Order("1",List.of( new Product("3","Affe"))),
+            new Order("2",List.of( new Product("7","Katze")))
+
+    ));
 
     public void addOrders(Order order) {
         this.orderList.add(order);
     }
 
-    public Order getOrderByID(String id) {
-        for (Order order : orderList) {
-            if(order.getId().equals(id)){
-                return order;
-            }
-        }
-        return null;
+    public List<Order> getOrderList(){
+        return orderList;
     }
 
 }
